03b45545608be2d43cb7e0681ebbb9747e400e53
任 <wy18434294023@163.com>
2025-04-25 10:48:35 +0800

    完善登陆页面

--- ren_demo/src/App.vue
+++ ren_demo/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <!-- 这里是所有页面的公共布局 -->
-  <router-view></router-view>
+  <div id="app">
+    <!-- 这里是所有页面的公共布局 -->
+    <router-view />
+  </div>
 </template>
 
 <script setup lang="ts">
