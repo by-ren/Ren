a8a126f5874c159c197fad16c5ef75ee0c205662
任 <wy18434294023@163.com>
2025-05-06 21:05:26 +0800

    完善项目配置文件

--- Ren-Demo/ren-admin/src/main/java/com/ren/RenApplication.java
+++ Ren-Demo/ren-admin/src/main/java/com/ren/RenApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.ren.**.system.mapper")
 public class RenApplication {
 
     public static void main(String[] args) {
